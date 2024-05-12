cc320e0c922d8658dd44cfda1ca7a87f09d15e0c
aung-khantkyaw <aungkhantkyaw.19-20@ucsmub.edu.mm>
2024-05-12 20:17:24 +0630

    update

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,7 +4,7 @@ import PortfolioCard from "./PortfolioCard";
 function Portfolio() {
   return  (
     <div className="container mx-auto mt-20"> 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 m-5">
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 m-5">
         {portfolio.map((project) => (
           <PortfolioCard {...project} key={project.id} />
         ))}
